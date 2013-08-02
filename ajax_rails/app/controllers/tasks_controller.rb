a4fed6d903e87828a70e64ff8e075f5cdf458878
eleyna <eleyna@gmail.com>
2013-08-01 22:44:25 -0400

    basics

--- ajax_rails/app/controllers/tasks_controller.rb
+++ ajax_rails/app/controllers/tasks_controller.rb
@@ -1,53 +1,37 @@
 class TasksController < ApplicationController
-	# STEP 3: Create the following TasksController actions:
-	# index - respond to html & json
-	# create - respond to json
-	# complete - respond to js
-	# destroy - respond to js
 
-def index
-	@tasks = Task.all
-	respond_to do |format|
-    format.html
-    format.json { render :json => @tasks.to_json }
-  end
-end
+	def index
+		@tasks = Task.all
+		respond_to do |format|
+    	format.html
+    	format.json { render :json => @tasks.to_json }
+  	end
+	end
 
-def create
+	def create
 		@task = Task.new(name: params[:task])
 		@task.save;
 
 		respond_to do |format|
 			format.json { render :json => @task }
 		end
-end
+	end
 
-
-
-def complete
-	@task = Task.find(params[:id])
-	@task.update_attribute(:completed, true)
-	@task.save
+	def complete
+		@task = Task.find(params[:id])
+		@task.update_attribute(:completed, true)
+		@task.save
 		respond_to do |format|
 			format.js
 		end
+	end
 
-end
-
-def destroy
-	@task = Task.find(params[:id])
+	def destroy
+		@task = Task.find(params[:id])
 		@task.destroy
-
 		respond_to do |format|
 			format.js
 		end
-end
+	end
 
-	# STEP 8: Make the index action respond to json requests with json containing the array from `Task.all`
-
-	# STEP 21: In the create action, instantiate a new `Task` object and save filling in the `name` parameter
-	# STEP 22: In the create action, set up `respond_to` to a json request with json containing the saved task object
-
-	# STEP 25: Find the task by id and set it's `completed` property to `true` in the `TasksController#complete` action
-	# STEP 26: Make the `TasksController#complete` `respond_to` respond to a javascript request
 end
--- ajax_rails/config/routes.rb
+++ ajax_rails/config/routes.rb
@@ -2,6 +2,7 @@ AjaxTodo::Application.routes.draw do
   root :to => 'tasks#index'
   get '/tasks/index' => 'tasks#index'
   post '/create/:task' => 'tasks#create'
+  get '/tasks/complete' => 'tasks#complete'
   put '/tasks/complete/:id' => 'tasks#complete'
   put '/tasks/delete/:id' => 'tasks#destroy'
 end
